0b165e69e875dd5b500406a2c7c9a54c08a2838d
PrimozBelej <pbelej90@gmail.com>
2016-12-14 07:14:02 +0100

    Initial version of genome parser.

--- genomereader.py
+++ genomereader.py
@@ -1,3 +1,14 @@
+"""Parsing and storing of genome data along with gene annotations.
+
+Attributes:
+    INT_FIELDS ({int}): Set of indices of fields in gene annotation
+        data that should be parsed as integers.
+    INT_LIST_FIELDS ({int}): Set of indices of fields in gene
+        annotation data that should be parsed as lists of integers. By
+        convention, these fields are comma separated and terminated by
+        an extra comma.
+"""
+
 from Bio import SeqIO
 
 
@@ -6,7 +17,17 @@ INT_LIST_FIELDS = {9, 10, 15}
 
 
 class GenomeReader:
+    """Provides parsing and storing of genomes and gene annotations.
 
+    Args:
+        genome_path (str): Path to a file with genome data.
+        genes_path (str): Path to a file with gene annotation data.
+
+    Attributes:
+        genome (str): Genome sequence.
+        genes ([dict]): List of dictionaries containing gene
+            annotations.
+    """
     def __init__(self, genome_path, genes_path):
         self.genome = None
         self.genes = None
@@ -14,9 +35,19 @@ class GenomeReader:
         self.load_genes(genes_path)
 
     def load_genome(self, path):
+        """Loads genome data from uncompressed fasta file.
+
+        Args:
+            path (str): Path to an uncompressed fasta file.
+        """
         self.genome = SeqIO.read(path, 'fasta').seq
 
     def load_genes(self, path):
+        """Loads gene annotation data from a csv file.
+        
+        Args:
+            path (str): Path to an uncompressed fasta file.
+        """
         self.genes = []
         with open(path, 'r') as in_file:
             labels = in_file.readline().strip().split()
